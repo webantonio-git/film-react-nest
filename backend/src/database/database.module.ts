23ed29c5bd54387aec61efd66efbf92651b25598
webantonio-git <webantonio@yandex.ru>
2026-01-27 14:00:08 +0300

    fix: regenerate package-lock for npm ci-6

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -17,37 +17,36 @@ import { ScheduleEntityOrm } from '../films/entities/schedule.entity';
         const url = process.env.DATABASE_URL || process.env.TYPEORM_URL;
 
 
-   const host =
-  process.env.POSTGRES_HOST ||
-  process.env.DATABASE_HOST ||
-  process.env.TYPEORM_HOST ||
-  '127.0.0.1';
+        const host =
+          process.env.DATABASE_HOST ||
+          process.env.POSTGRES_HOST ||
+          process.env.TYPEORM_HOST ||
+          '127.0.0.1';
 
-const port = Number(
-  process.env.POSTGRES_PORT ||
-    process.env.DATABASE_PORT ||
-    process.env.TYPEORM_PORT ||
-    '5432',
-);
+        const port = Number(
+          process.env.DATABASE_PORT ||
+            process.env.POSTGRES_PORT ||
+            process.env.TYPEORM_PORT ||
+            '5432',
+        );
 
-const username =
-  process.env.POSTGRES_USER ||
-  process.env.DATABASE_USERNAME ||
-  process.env.TYPEORM_USERNAME ||
-  'postgres';
+        const username =
+          process.env.DATABASE_USERNAME ||
+          process.env.POSTGRES_USER ||
+          process.env.TYPEORM_USERNAME ||
+          'postgres';
 
-const password =
-  process.env.POSTGRES_PASSWORD ||
-  process.env.DATABASE_PASSWORD ||
-  process.env.TYPEORM_PASSWORD ||
-  'postgres';
+        const password =
+          process.env.DATABASE_PASSWORD ||
+          process.env.POSTGRES_PASSWORD ||
+          process.env.TYPEORM_PASSWORD ||
+          'postgres';
 
-const database =
-  process.env.POSTGRES_DB ||
-  process.env.DATABASE_NAME ||
-  process.env.TYPEORM_DATABASE ||
-  'postgres';
-
+        const database =
+          process.env.DATABASE_NAME ||
+          process.env.POSTGRES_DB ||
+          process.env.TYPEORM_DATABASE ||
+          'postgres';
 
         const synchronize = process.env.TYPEORM_SYNCHRONIZE === 'true';
         const logging = process.env.TYPEORM_LOGGING === 'true';
